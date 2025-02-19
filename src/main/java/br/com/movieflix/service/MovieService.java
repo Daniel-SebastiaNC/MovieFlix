e769e9c348d45334e4a41f0f05461819386dc45b
Daniel-SebastiaNC <daniel.sebastian.nc@gmail.com>
2025-02-19 14:10:08 -0300

    fix: Adicona a resposta para o metodo POST de salvar movie, usa para buscar categories e streamings no banco para o retorno de resposta completo por todos os dados no momento em que é adiconado um movie no DB

--- src/main/java/br/com/movieflix/service/MovieService.java
+++ src/main/java/br/com/movieflix/service/MovieService.java
@@ -2,13 +2,18 @@ package br.com.movieflix.service;
 
 import br.com.movieflix.controller.request.MovieRequest;
 import br.com.movieflix.controller.response.MovieResponse;
+import br.com.movieflix.entity.Category;
 import br.com.movieflix.entity.Movie;
+import br.com.movieflix.entity.Streaming;
 import br.com.movieflix.mapper.MovieMapper;
+import br.com.movieflix.repository.CategoryRepository;
 import br.com.movieflix.repository.MovieRepository;
+import br.com.movieflix.repository.StreamingRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -16,18 +21,45 @@ import java.util.List;
 @RequiredArgsConstructor
 public class MovieService {
 
-    private final MovieRepository repository;
+    private final MovieRepository movieRepository;
+    private final CategoryRepository categoryRepository;
+    private final StreamingRepository streamingRepository;
 
     public MovieResponse save(MovieRequest movieRequest){
+        //Mapper Movie
         Movie movie = MovieMapper.toMovie(movieRequest);
-        Movie saveMovie = repository.save(movie);
+
+        //Setter Categories and Streamings
+        movie.setCategories(this.findCategories(movie.getCategories()));
+        movie.setStreamings(this.findstreamings(movie.getStreamings()));
+
+        //Save
+        Movie saveMovie = movieRepository.save(movie);
+
+        //Mapper Response
         return MovieMapper.toMovieResponse(saveMovie);
     }
 
     public List<MovieResponse> getAllMovies(){
-        List<Movie> movies = repository.findAll();
+        List<Movie> movies = movieRepository.findAll();
         return movies.stream()
                 .map(MovieMapper::toMovieResponse)
                 .toList();
+    }
+
+    private List<Category> findCategories(List<Category> categories){
+        List<Category> categoryFound = new ArrayList<>();
+
+        categories.forEach(category -> categoryRepository.findById(category.getId()).ifPresent(categoryFound::add));
+
+        return categoryFound;
+    }
+
+    private List<Streaming> findstreamings(List<Streaming> streamings){
+        List<Streaming> streamingsFound = new ArrayList<>();
+
+        streamings.forEach(streaming -> streamingRepository.findById(streaming.getId()).ifPresent(streamingsFound::add));
+
+        return streamingsFound;
     }
 }
