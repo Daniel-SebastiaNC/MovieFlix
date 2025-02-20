e683e3bc7b5797a64b024ccd577b74be75faf457
Daniel-SebastiaNC <daniel.sebastian.nc@gmail.com>
2025-02-20 12:07:33 -0300

    feat: adicona AuthController com metodo POST para registrar User, junto com UserService e UserRepository

--- src/main/java/br/com/movieflix/controller/AuthController.java
+++ src/main/java/br/com/movieflix/controller/AuthController.java
@@ -1,4 +1,25 @@
 package br.com.movieflix.controller;
 
+import br.com.movieflix.controller.request.UserRequest;
+import br.com.movieflix.controller.response.UserRegisterResponse;
+import br.com.movieflix.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movieflix/auth")
+@RequiredArgsConstructor
 public class AuthController {
+
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<UserRegisterResponse> register(@RequestBody UserRequest userRequest) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.register(userRequest));
+    }
 }
--- src/main/java/br/com/movieflix/service/UserService.java
+++ src/main/java/br/com/movieflix/service/UserService.java
@@ -1,4 +1,21 @@
 package br.com.movieflix.service;
 
+import br.com.movieflix.controller.request.UserRequest;
+import br.com.movieflix.controller.response.UserRegisterResponse;
+import br.com.movieflix.entity.User;
+import br.com.movieflix.mapper.UserMapper;
+import br.com.movieflix.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
+
+    public UserRegisterResponse register(UserRequest userRequest){
+        User user = UserMapper.toUser(userRequest);
+        User saveUser = userRepository.save(user);
+        return UserMapper.toUserRegisterResponse(saveUser);
+    }
 }
