7ec2ef2ea57dee3fe0216854e0acd3098e9a25fc
Daniel-SebastiaNC <daniel.sebastian.nc@gmail.com>
2025-02-20 12:03:07 -0300

    feat: adicona a entidade User e sua migration para criacão da tabela

---
+++ src/main/java/br/com/movieflix/entity/User.java
@@ -0,0 +1,26 @@
+package br.com.movieflix.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Builder
+@Entity
+@Table(name = "tb_user")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
